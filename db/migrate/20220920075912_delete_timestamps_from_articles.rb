24afaead68c334526af9a8d52d0246a39e7d61ce
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-20 12:10:28 +0300

    init(alpha-blog): init project, create articles resource

---
+++ db/migrate/20220920075912_delete_timestamps_from_articles.rb
@@ -0,0 +1,6 @@
+class DeleteTimestampsFromArticles < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :articles, :created_at, :datetime
+    remove_column :articles, :updated_at, :datetime
+  end
+end
