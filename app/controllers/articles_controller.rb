34dbe371caabfed43f99b704d1e040a3969ac303
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-21 11:30:22 +0300

    feat(apha-blog): add destroy method, improve layouts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,36 +1,49 @@
 class ArticlesController < ApplicationController
+  before_action :find_article_with_params_id, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new; end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
     else
-      render 'new'
+      render 'new', status: :bad_request
     end
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(params.require(:article).permit(:title, :description))
+    @article.update(article_params)
     if @article.save
       redirect_to @article
       flash[:notice] = 'Article has been updated'
     else
-      render 'edit'
+      render 'edit', status: :bad_request
     end
+  end
+
+  def destroy
+    redirect_to(articles_path)
+    @article.destroy
+    flash[:notice] = "Article #{@article.id} has been destroyed"
+  end
+
+  private
+
+  def find_article_with_params_id
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
