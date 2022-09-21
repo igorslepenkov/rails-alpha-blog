0fad7f643538501bc04d5c59eccd2e31409d73af
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-21 09:44:59 +0300

    feat(develop): implement create and edit forms and actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,31 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new; end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article
+      flash[:notice] = 'Article has been updated'
+    else
+      render 'edit'
+    end
+  end
 end
