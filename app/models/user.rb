95d89f54b797a481f39b32186bf495a94c13b0e8
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-22 13:04:43 +0300

    feat(user-articles-association): add one-to-many association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)\z/i
+  has_many :articles
 
+  VALID_EMAIL_REGEX = /\A([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)\z/i
   validates :username,
             presence: true,
             length: { in: 3..25 },
