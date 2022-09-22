2f258816efd32d3a943b361ef7d39b11fcc828fc
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-22 12:36:19 +0300

    feat(user-validations): add user-validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)\z/i
+
+  validates :username,
+            presence: true,
+            length: { in: 3..25 },
+            uniqueness: { case_sensitive: false }
+
+  validates :email,
+            presence: true,
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
